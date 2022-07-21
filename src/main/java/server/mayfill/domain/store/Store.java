8e9c5ba6f3732676aa14b46e92cad30ceae91314
chanwoo <chocw0402@gmail.com>
2022-07-22 02:50:38 +0900

    [Feat] store 엔티티 개발

--- src/main/java/server/mayfill/domain/store/Store.java
+++ src/main/java/server/mayfill/domain/store/Store.java
@@ -1,2 +1,62 @@
-package server.mayfill.domain.store;public class Store {
+package server.mayfill.domain.store;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import server.mayfill.domain.common.AuditingTimeEntity;
+import server.mayfill.domain.store.embedded.Address;
+import server.mayfill.domain.store.embedded.Coordinate;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Store extends AuditingTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Embedded
+    private Address address;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "web_site")
+    private String webSite;
+
+    @Embedded
+    private Coordinate coordinate;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    private Store(String name, String description, Address address, String phoneNumber, String webSite, Coordinate coordinate) {
+        this.name = name;
+        this.description = description;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.webSite = webSite;
+        this.coordinate = coordinate;
+    }
+
+    public static Store of(String name, String description, Address address, String phoneNumber, String webSite, Coordinate coordinate) {
+        return Store.builder()
+                .name(name)
+                .description(description)
+                .address(address)
+                .phoneNumber(phoneNumber)
+                .webSite(webSite)
+                .coordinate(coordinate)
+                .build();
+    }
+
 }
