5818d239ad26aa5493b5baac717cabd6240b496f
chanwoo <chocw0402@gmail.com>
2022-07-21 23:58:29 +0900

    initial commit

---
+++ src/main/java/server/mayfill/MayfillApplication.java
@@ -0,0 +1,13 @@
+package server.mayfill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayfillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MayfillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/mayfill/MayfillApplicationTests.java
@@ -0,0 +1,13 @@
+package server.mayfill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayfillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
