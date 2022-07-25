9dcabe0c15f07e55e1a3fc148f0c3ddb5cf55f28
chanwoo <chocw0402@gmail.com>
2022-07-25 19:52:30 +0900

    [Feat] apple public key api

--- src/main/java/server/mayfill/external/client/apple/AppleAuthApiClient.java
+++ src/main/java/server/mayfill/external/client/apple/AppleAuthApiClient.java
@@ -1,2 +1,13 @@
-package server.mayfill.external.client.apple;public class AppleApiClient {
+package server.mayfill.external.client.apple;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import server.mayfill.external.client.apple.dto.ApplePublicKeyResponse;
+
+@FeignClient(name = "appleAuthApiClient", url = "https://appleid.apple.com/auth")
+public interface AppleAuthApiClient {
+
+    @GetMapping("/keys")
+    ApplePublicKeyResponse getAppleAuthPublicKey();
+
 }
