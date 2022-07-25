1286cd8e9698c8663a7c4432b43821244317a930
chanwoo <chocw0402@gmail.com>
2022-07-25 19:53:26 +0900

    [Feat] apple idToken decode

--- src/main/java/server/mayfill/external/client/apple/AppleTokenDecoder.java
+++ src/main/java/server/mayfill/external/client/apple/AppleTokenDecoder.java
@@ -1,2 +1,9 @@
-package server.mayfill.external.client.apple;public interface AppleTokenDecoder {
+package server.mayfill.external.client.apple;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface AppleTokenDecoder {
+
+    String getSocialIdFromIdToken(@NotNull String idToken);
+
 }
