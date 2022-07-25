d4964ceb16b0c6af406acc94cee1ba71e1e6604d
chanwoo <chocw0402@gmail.com>
2022-07-25 23:32:12 +0900

    [Feat] generic apiResponse

--- src/main/java/server/mayfill/common/dto/ApiResponse.java
+++ src/main/java/server/mayfill/common/dto/ApiResponse.java
@@ -1,2 +1,39 @@
-package server.mayfill.common.dto;public class ApiResponse {
+package server.mayfill.common.dto;
+
+import lombok.*;
+import server.mayfill.common.exception.ResponseResult;
+import server.mayfill.common.exception.StatusCode;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    public static final ApiResponse<String> SUCCESS = success("");
+
+    private StatusCode statusCode;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(StatusCode.OK, "", data);
+    }
+
+    public static <T> ApiResponse<T> success(ResponseResult responseResult) {
+        return new ApiResponse<>(responseResult.getStatusCode(), responseResult.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> success(ResponseResult responseResult, T data) {
+        return new ApiResponse<>(responseResult.getStatusCode(), responseResult.getMessage(), data);
+    }
+
+    public static <T> ApiResponse<T> error(ResponseResult responseResult) {
+        return new ApiResponse<>(responseResult.getStatusCode(), responseResult.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> error(ResponseResult responseResult, String message) {
+        return new ApiResponse<>(responseResult.getStatusCode(), message, null);
+    }
+
 }
