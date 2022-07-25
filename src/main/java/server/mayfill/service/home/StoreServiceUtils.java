32d6c44dd8a093f786ec7a47e6a33aace06b6d8d
chanwoo <chocw0402@gmail.com>
2022-07-25 23:59:05 +0900

    [Feat] store service utils

--- src/main/java/server/mayfill/service/home/StoreServiceUtils.java
+++ src/main/java/server/mayfill/service/home/StoreServiceUtils.java
@@ -1,2 +1,18 @@
-package server.mayfill.service.home;public class StoreServiceUtils {
+package server.mayfill.service.home;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import server.mayfill.common.exception.ResponseResult;
+import server.mayfill.common.exception.custom.NotFoundException;
+import server.mayfill.domain.store.repository.StoreRepository;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StoreServiceUtils {
+
+    public static void validateNotExistsStore(StoreRepository storeRepository, Long storeId) {
+        if (!storeRepository.existsById(storeId)) {
+            throw new NotFoundException(String.format("해당하는 리필 스테이션 (%s) 이 존재하지 않습니다", storeId), ResponseResult.NOT_FOUND_EXCEPTION);
+        }
+    }
+
 }
