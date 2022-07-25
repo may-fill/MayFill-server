7a189f71c4a7b5f7d2a4b74159eab5e8a65afcff
chanwoo <chocw0402@gmail.com>
2022-07-25 23:47:25 +0900

    [Feat] 로그인된 유저 아이디 resolver 등록

--- src/main/java/server/mayfill/config/resolver/LoginUserIdResolver.java
+++ src/main/java/server/mayfill/config/resolver/LoginUserIdResolver.java
@@ -1,2 +1,35 @@
-package server.mayfill.config.resolver;public class LoginUserIdResolver {
+package server.mayfill.config.resolver;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import server.mayfill.common.exception.custom.InternalServerException;
+import server.mayfill.config.interceptor.Auth;
+import server.mayfill.config.security.JwtConstants;
+
+@Component
+public class LoginUserIdResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUserId.class) && parameter.getParameterType().equals(Long.class);
+    }
+
+    @NotNull
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, @NotNull NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        if (parameter.getMethodAnnotation(Auth.class) == null) {
+            throw new InternalServerException("@Auth 어노테이션이 필요한 컨트롤러입니다");
+        }
+        Object object = webRequest.getAttribute(JwtConstants.USER_ID, 0);
+        if (object == null) {
+            throw new InternalServerException(String.format("USER_ID 를 가져오지 못했습니. (%s - %s)", parameter.getClass(), parameter.getMethod()));
+        }
+        return object;
+    }
+
 }
