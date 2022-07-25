0025cc3d61cc53ebdd6ef877ff9340a001ac7478
chanwoo <chocw0402@gmail.com>
2022-07-25 23:42:12 +0900

    [Feat] 설정한 interceptor, resolver 등록

--- src/main/java/server/mayfill/config/WebConfig.java
+++ src/main/java/server/mayfill/config/WebConfig.java
@@ -1,2 +1,30 @@
-package server.mayfill.config;public class WebConfig {
+package server.mayfill.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import server.mayfill.config.interceptor.AuthInterceptor;
+import server.mayfill.config.resolver.LoginUserIdResolver;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final LoginUserIdResolver loginUserIdResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserIdResolver);
+    }
+
 }
