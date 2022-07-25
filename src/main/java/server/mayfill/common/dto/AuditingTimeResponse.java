f3531b6a24569d0f786558a929885efc8548bab1
chanwoo <chocw0402@gmail.com>
2022-07-25 23:32:48 +0900

    [Feat] auditing time response

--- src/main/java/server/mayfill/common/dto/AuditingTimeResponse.java
+++ src/main/java/server/mayfill/common/dto/AuditingTimeResponse.java
@@ -1,2 +1,25 @@
-package server.mayfill.common.dto;public class AuditingTimeResponse {
+package server.mayfill.common.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import server.mayfill.domain.common.AuditingTimeEntity;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class AuditingTimeResponse {
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    protected void setBaseTime(AuditingTimeEntity auditingTimeEntity) {
+        this.createdAt = auditingTimeEntity.getCreatedAt();
+        this.updatedAt = auditingTimeEntity.getUpdatedAt();
+    }
+
 }
