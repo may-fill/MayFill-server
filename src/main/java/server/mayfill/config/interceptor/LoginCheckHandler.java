bf42c7c1a2f5a1ade90502f588b5d6ee0912ae26
chanwoo <chocw0402@gmail.com>
2022-07-25 23:45:55 +0900

    [Feat] 로그인 된 유저 확인

--- src/main/java/server/mayfill/config/interceptor/LoginCheckHandler.java
+++ src/main/java/server/mayfill/config/interceptor/LoginCheckHandler.java
@@ -1,2 +1,32 @@
-package server.mayfill.config.interceptor;public class LoginCheckHandler {
+package server.mayfill.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import server.mayfill.common.exception.custom.UnAuthorizeException;
+import server.mayfill.common.util.HttpHeaderUtils;
+import server.mayfill.common.util.JwtUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@RequiredArgsConstructor
+public class LoginCheckHandler {
+
+    private final JwtUtils jwtProvider;
+
+    public Long getUserId(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HttpHeaderUtils.AUTH_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(HttpHeaderUtils.BEARER_TOKEN)) {
+            String accessToken = bearerToken.substring(HttpHeaderUtils.BEARER_TOKEN.length());
+            if (jwtProvider.validateToken(accessToken)) {
+                Long userId = jwtProvider.getUserIdFromJwt(accessToken);
+                if (userId != null) {
+                    return userId;
+                }
+            }
+        }
+        throw new UnAuthorizeException(String.format("잘못된 JWT (%s) 입니다", bearerToken));
+    }
+
 }
