d4f50dab1f1a4f55655497fb81f4943542168db6
chanwoo <chocw0402@gmail.com>
2022-07-25 19:55:39 +0900

    [Feat] kakao api

--- src/main/java/server/mayfill/external/client/kakao/KakaoApiClient.java
+++ src/main/java/server/mayfill/external/client/kakao/KakaoApiClient.java
@@ -1,2 +1,14 @@
-package server.mayfill.external.client.kakao;public interface KakaoApiClient {
+package server.mayfill.external.client.kakao;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import server.mayfill.external.client.kakao.dto.KakaoAuthResponse;
+
+@FeignClient(name = "kakaoAuthApiClient", url = "https://kapi.kakao.com")
+public interface KakaoApiClient {
+
+    @GetMapping("/v2/user/me")
+    KakaoAuthResponse getKakaoUserProfile(@RequestHeader("Authorization") String accessToken);
+
 }
