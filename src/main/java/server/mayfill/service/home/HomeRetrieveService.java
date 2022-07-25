00e1cf8da5c045ba4e1f985c146c56db2aec802b
chanwoo <chocw0402@gmail.com>
2022-07-25 23:58:15 +0900

    [Feat] 메인 뷰 serivce

--- src/main/java/server/mayfill/service/home/HomeRetrieveService.java
+++ src/main/java/server/mayfill/service/home/HomeRetrieveService.java
@@ -1,2 +1,31 @@
-package server.mayfill.service.home;public class HomeRetirveService {
+package server.mayfill.service.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.mayfill.domain.store.repository.StoreRepository;
+import server.mayfill.service.home.dto.response.AllStoreResponse;
+import server.mayfill.service.home.dto.response.OneStoreResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class HomeRetrieveService {
+
+    private final StoreRepository storeRepository;
+
+    public List<AllStoreResponse> retrieveAllStore() {
+        return storeRepository.findAllStore()
+                .stream()
+                .map(AllStoreResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public OneStoreResponse retrieveOneStore(Long storeId) {
+        return OneStoreResponse.of(storeRepository.findStoreByStoreId(storeId));
+    }
+
 }
