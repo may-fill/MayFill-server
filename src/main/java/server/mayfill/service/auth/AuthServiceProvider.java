18a0b4d659ea15a27aae2e36bbc700b9a3d00614
chanwoo <chocw0402@gmail.com>
2022-07-26 00:02:28 +0900

    [Feat] social type 에 따른 auth service provider

--- src/main/java/server/mayfill/service/auth/AuthServiceProvider.java
+++ src/main/java/server/mayfill/service/auth/AuthServiceProvider.java
@@ -1,2 +1,32 @@
-package server.mayfill.service.auth;public class AuthServiceProvider {
+package server.mayfill.service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import server.mayfill.domain.user.enumerate.SocialType;
+import server.mayfill.service.auth.impl.AppleAuthService;
+import server.mayfill.service.auth.impl.KakaoAuthService;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AuthServiceProvider {
+
+    private static final Map<SocialType, AuthService> authServiceMap = new HashMap<>();
+
+    private final AppleAuthService appleAuthService;
+    private final KakaoAuthService kakaoAuthService;
+
+    @PostConstruct // 의존성 주입이 완료된 후 초기화
+    public void initAuthServiceMap() {
+        authServiceMap.put(SocialType.APPLE, appleAuthService);
+        authServiceMap.put(SocialType.KAKAO, kakaoAuthService);
+    }
+
+    public AuthService getAuthService(SocialType socialType) {
+        return authServiceMap.get(socialType);
+    }
+
 }
