9f29b6a40d1af616194489dff5a02a381176770f
chanwoo <chocw0402@gmail.com>
2022-07-25 23:55:21 +0900

    [Feat] review service 레이어 dto

--- src/main/java/server/mayfill/service/review/dto/response/ReviewResponse.java
+++ src/main/java/server/mayfill/service/review/dto/response/ReviewResponse.java
@@ -1,2 +1,34 @@
-package server.mayfill.service.review.dto.response;public class ReviewResponse {
+package server.mayfill.service.review.dto.response;
+
+import lombok.*;
+import server.mayfill.common.dto.AuditingTimeResponse;
+import server.mayfill.domain.review.Review;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ReviewResponse extends AuditingTimeResponse {
+
+    private Long reviewId;
+    private String nickname;
+    private String comments;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    public ReviewResponse(String nickname, Long reviewId, String comments) {
+        this.nickname = nickname;
+        this.reviewId = reviewId;
+        this.comments = comments;
+    }
+
+    public static ReviewResponse of(String nickname, Review review) {
+        ReviewResponse response = ReviewResponse.builder()
+                .reviewId(review.getId())
+                .nickname(nickname)
+                .comments(review.getComments())
+                .build();
+        response.setBaseTime(review);
+        return response;
+    }
+
 }
+
