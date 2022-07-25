26db0cc42828f4219492e55403a6a42806af7aca
chanwoo <chocw0402@gmail.com>
2022-07-26 00:00:21 +0900

    [Feat] auth service interface

--- src/main/java/server/mayfill/service/auth/AuthService.java
+++ src/main/java/server/mayfill/service/auth/AuthService.java
@@ -1,2 +1,8 @@
-package server.mayfill.service.auth;public interface AuthService {
+package server.mayfill.service.auth;
+
+import server.mayfill.domain.user.User;
+import server.mayfill.service.auth.dto.request.LoginDto;
+
+public interface AuthService {
+    User socialLogin(LoginDto request);
 }
