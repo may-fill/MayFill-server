e476d4a2678f741a7a37f51d784c8876492105fe
chanwoo <chocw0402@gmail.com>
2022-07-26 00:03:10 +0900

    [Add] enable 설정

--- src/main/java/server/mayfill/MayfillApplication.java
+++ src/main/java/server/mayfill/MayfillApplication.java
@@ -2,7 +2,13 @@ package server.mayfill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableFeignClients
+@EnableJpaAuditing
+@EnableSwagger2
 @SpringBootApplication
 public class MayfillApplication {
 
