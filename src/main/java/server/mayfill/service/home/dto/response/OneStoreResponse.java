866bd3f473795409ee548486a0bd9b2f06b0f91d
chanwoo <chocw0402@gmail.com>
2022-07-25 23:56:13 +0900

    [Feat] home service 레이어 dto

--- src/main/java/server/mayfill/service/home/dto/response/OneStoreResponse.java
+++ src/main/java/server/mayfill/service/home/dto/response/OneStoreResponse.java
@@ -1,2 +1,39 @@
-package server.mayfill.service.home.dto.response;public class OneStoreResponse {
+package server.mayfill.service.home.dto.response;
+
+import lombok.*;
+import server.mayfill.domain.store.Store;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class OneStoreResponse {
+
+    private Long storeId;
+    private String storeName;
+    private String address;
+    private String phoneNumber;
+    private String webSite;
+    private String description;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    private OneStoreResponse(Long storeId, String storeName, String address, String phoneNumber, String webSite, String description) {
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.webSite = webSite;
+        this.description = description;
+    }
+
+    public static OneStoreResponse of(Store store) {
+        return OneStoreResponse.builder()
+                .storeId(store.getId())
+                .storeName(store.getName())
+                .address(store.getAddress().getJibun() + store.getAddress().getRoad() + store.getAddress().getDetail())
+                .phoneNumber(store.getPhoneNumber())
+                .webSite(store.getWebSite())
+                .description(store.getDescription())
+                .build();
+    }
+
 }
