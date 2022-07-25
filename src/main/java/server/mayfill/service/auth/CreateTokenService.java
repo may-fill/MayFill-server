67056caadb4467f80e025abe7d0c0dc063fb7442
chanwoo <chocw0402@gmail.com>
2022-07-25 23:59:59 +0900

    [Feat] token 생성 service

--- src/main/java/server/mayfill/service/auth/CreateTokenService.java
+++ src/main/java/server/mayfill/service/auth/CreateTokenService.java
@@ -1,2 +1,26 @@
-package server.mayfill.service.auth;public class CreateTokenService {
+package server.mayfill.service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.mayfill.common.exception.custom.UnAuthorizeException;
+import server.mayfill.common.util.JwtUtils;
+import server.mayfill.controller.auth.dto.request.TokenRequest;
+import server.mayfill.service.auth.dto.response.TokenResponseDto;
+
+@Service
+@RequiredArgsConstructor
+public class CreateTokenService {
+
+    private final JwtUtils jwtProvider;
+
+    @Transactional
+    public TokenResponseDto reissueToken(TokenRequest request) {
+        if (!jwtProvider.validateToken(request.getRefreshToken())) {
+            throw new UnAuthorizeException(String.format("리프레시 토큰 (%s) 이 유효하지 않습니다", request.getRefreshToken()));
+        }
+        Long userId = jwtProvider.getUserIdFromJwt(request.getAccessToken());
+        return jwtProvider.createTokenByUserId(userId);
+    }
+
 }
