3ad205f1f2fc5abec9ea61795ac7156e5aaafd60
chanwoo <chocw0402@gmail.com>
2022-07-26 02:06:35 +0900

    [Add] 후기 등록, 조회

--- src/main/java/server/mayfill/controller/review/ReviewController.java
+++ src/main/java/server/mayfill/controller/review/ReviewController.java
@@ -1,23 +1,39 @@
 package server.mayfill.controller.review;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import server.mayfill.common.dto.ApiResponse;
+import server.mayfill.common.exception.ResponseResult;
+import server.mayfill.config.interceptor.Auth;
 import server.mayfill.config.resolver.LoginUserId;
+import server.mayfill.service.review.ReviewService;
 import server.mayfill.service.review.dto.request.AddReviewRequestDto;
+import server.mayfill.service.review.dto.response.ReviewResponse;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
 
-//    @PostMapping("/v1/store/review")
-//    public ApiResponse<> addReview(@Valid @RequestBody AddReviewRequestDto request, @ApiIgnore @LoginUserId Long userId) {
-//
-//    }
+    private final ReviewService reviewService;
+
+    @ApiOperation("[인증] 리필 스테이션 상세보기 페이지 - 후기 등록")
+    @Auth
+    @PostMapping("/v1/store/review")
+    public ApiResponse<ResponseResult> addReview(@Valid @RequestBody AddReviewRequestDto request, @ApiIgnore @LoginUserId Long userId) {
+        reviewService.addReview(request, userId);
+        return ApiResponse.success(ResponseResult.SUCCESS_CREATED_REVIEW_COMMENT);
+    }
+
+    @ApiOperation("[인증] 리필 스테이션 상세보기 페이지 - 후기 조회")
+    @Auth
+    @GetMapping("/v1/store/{storeId}/review")
+    public ApiResponse<List<ReviewResponse>> retrieveReview(@PathVariable Long storeId) {
+        return ApiResponse.success(ResponseResult.OK_RETRIEVE_STORE_REVIEW, reviewService.retrieveReview(storeId));
+    }
 
 }
