b2075577cb2ab920c76e9b51b3ec10fed15206a2
chanwoo <chocw0402@gmail.com>
2022-07-25 23:54:05 +0900

    [Feat] review controller

--- src/main/java/server/mayfill/controller/review/ReviewController.java
+++ src/main/java/server/mayfill/controller/review/ReviewController.java
@@ -1,2 +1,23 @@
-package server.mayfill.controller.review;public class ReviewController {
+package server.mayfill.controller.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import server.mayfill.common.dto.ApiResponse;
+import server.mayfill.config.resolver.LoginUserId;
+import server.mayfill.service.review.dto.request.AddReviewRequestDto;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+
+//    @PostMapping("/v1/store/review")
+//    public ApiResponse<> addReview(@Valid @RequestBody AddReviewRequestDto request, @ApiIgnore @LoginUserId Long userId) {
+//
+//    }
+
 }
