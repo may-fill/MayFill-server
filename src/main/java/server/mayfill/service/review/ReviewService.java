1148454e52b412316b4550b38ce7894325488b51
chanwoo <chocw0402@gmail.com>
2022-07-25 23:55:45 +0900

    [Feat] review serivce

--- src/main/java/server/mayfill/service/review/ReviewService.java
+++ src/main/java/server/mayfill/service/review/ReviewService.java
@@ -1,2 +1,35 @@
-package server.mayfill.service.review;public class ReviewService {
+package server.mayfill.service.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.mayfill.domain.review.repository.ReviewRepository;
+import server.mayfill.domain.store.repository.StoreRepository;
+import server.mayfill.domain.user.repository.UserRepository;
+import server.mayfill.service.home.StoreServiceUtils;
+import server.mayfill.service.review.dto.request.AddReviewRequestDto;
+import server.mayfill.service.review.dto.response.ReviewResponse;
+import server.mayfill.service.user.UserServiceUtils;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final StoreRepository storeRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void addReview(AddReviewRequestDto request, Long userId) {
+        StoreServiceUtils.validateNotExistsStore(storeRepository, request.getStoreId());
+        reviewRepository.save(request.toEntity(
+                storeRepository.findStoreByStoreId(request.getStoreId()),
+                UserServiceUtils.findUserById(userRepository, userId))
+        );
+    }
+
+//    public List<ReviewResponse> retrieveReview
+
 }
