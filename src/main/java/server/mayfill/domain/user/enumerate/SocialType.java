d2cd83bf7760e37394c9ab3e245a8ba27c22b276
chanwoo <chocw0402@gmail.com>
2022-07-25 19:57:36 +0900

    [Feat] user entity 소셜 타입

--- src/main/java/server/mayfill/domain/user/enumerate/SocialType.java
+++ src/main/java/server/mayfill/domain/user/enumerate/SocialType.java
@@ -1,4 +1,17 @@
 package server.mayfill.domain.user.enumerate;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SocialType {
+
+    APPLE("애플"),
+    KAKAO("카카오")
+    ;
+
+    private final String value;
+
 }
