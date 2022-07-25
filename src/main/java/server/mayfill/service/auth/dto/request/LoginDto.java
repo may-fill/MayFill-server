20c42d86d046131b1568ced2e0f8ef7996f2634d
chanwoo <chocw0402@gmail.com>
2022-07-26 00:01:56 +0900

    [Feat] auth service 레이어 dto

--- src/main/java/server/mayfill/service/auth/dto/request/LoginDto.java
+++ src/main/java/server/mayfill/service/auth/dto/request/LoginDto.java
@@ -1,2 +1,24 @@
-package server.mayfill.service.auth.dto.request;public class LoginDto {
+package server.mayfill.service.auth.dto.request;
+
+import lombok.*;
+import server.mayfill.domain.user.enumerate.SocialType;
+import server.mayfill.service.user.dto.request.CreateUserDto;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class LoginDto {
+
+    private String socialAccessToken;
+    private SocialType socialType;
+
+    public static LoginDto of(String socialAccessToken, SocialType socialType) {
+        return new LoginDto(socialAccessToken, socialType);
+    }
+
+    public CreateUserDto toCreateUserDto(String socialId, String nickname) {
+        return CreateUserDto.of(nickname, socialId, socialType);
+    }
+
 }
--- src/main/java/server/mayfill/service/auth/dto/response/TokenResponseDto.java
+++ src/main/java/server/mayfill/service/auth/dto/response/TokenResponseDto.java
@@ -1,2 +1,18 @@
-package server.mayfill.service.auth.dto.response;public class TokenResponseDto {
+package server.mayfill.service.auth.dto.response;
+
+import lombok.*;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TokenResponseDto {
+
+    private String accessToken;
+    private String refreshToken;
+
+    public static TokenResponseDto of(String accessToken, String refreshToken) {
+        return new TokenResponseDto(accessToken, refreshToken);
+    }
+
 }
