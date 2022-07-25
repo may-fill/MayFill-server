2c22d47e02bb2e18ad2d01bccd8d2db610e9930b
chanwoo <chocw0402@gmail.com>
2022-07-25 23:54:43 +0900

    [Feat] user service 레이어 dto

--- src/main/java/server/mayfill/service/user/dto/request/CreateUserDto.java
+++ src/main/java/server/mayfill/service/user/dto/request/CreateUserDto.java
@@ -1,2 +1,20 @@
-package server.mayfill.service.user.dto.request;public class CreateUserDto {
+package server.mayfill.service.user.dto.request;
+
+import lombok.*;
+import server.mayfill.domain.user.enumerate.SocialType;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CreateUserDto {
+
+    private String nickname;
+    private String socialId;
+    private SocialType socialType;
+
+    public static CreateUserDto of(String nickname, String socialId, SocialType socialType) {
+        return new CreateUserDto(nickname, socialId, socialType);
+    }
+
 }
