9f8ea31002e3e75f503b3526e72aa687c3faa5c8
chanwoo <chocw0402@gmail.com>
2022-07-25 23:55:00 +0900

    [Feat] user serivce

--- src/main/java/server/mayfill/service/user/UserService.java
+++ src/main/java/server/mayfill/service/user/UserService.java
@@ -1,2 +1,26 @@
-package server.mayfill.service.user;public class UserService {
+package server.mayfill.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.mayfill.domain.user.User;
+import server.mayfill.domain.user.repository.UserRepository;
+import server.mayfill.service.user.dto.request.CreateUserDto;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User registerUser(CreateUserDto request) {
+        return userRepository.save(User.newInstance(request.getSocialId(), request.getSocialType(), request.getNickname()));
+    }
+
+    @Transactional
+    public void resign(Long userId) {
+        userRepository.delete(UserServiceUtils.findUserById(userRepository, userId));
+    }
+
 }
--- src/main/java/server/mayfill/service/user/UserServiceUtils.java
+++ src/main/java/server/mayfill/service/user/UserServiceUtils.java
@@ -1,2 +1,28 @@
-package server.mayfill.service.user;public class UserServiceUtils {
+package server.mayfill.service.user;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import server.mayfill.common.exception.ResponseResult;
+import server.mayfill.common.exception.custom.NotFoundException;
+import server.mayfill.domain.user.User;
+import server.mayfill.domain.user.enumerate.SocialType;
+import server.mayfill.domain.user.repository.UserRepository;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserServiceUtils {
+
+    @NotNull
+    public static User findUserById(UserRepository userRepository, Long userId) {
+        User user = userRepository.findUserById(userId);
+        if (user == null) {
+            throw new NotFoundException(String.format("존재하지 않는 유저 (%s) 입니다", userId), ResponseResult.NOT_FOUND_EXCEPTION);
+        }
+        return user;
+    }
+
+    public static User findUserBySocialIdAndSocialType(UserRepository userRepository, String socialId, SocialType socialType) {
+        return userRepository.findUserBySocialIdAndSocialType(socialId, socialType);
+    }
+
 }
