939b7c19cc0168cd35a25714e57345cf820d0927
chanwoo <chocw0402@gmail.com>
2022-07-28 16:11:35 +0900

    [Feat] naver map api geo code response dto

--- src/main/java/server/mayfill/external/client/naver/dto/NaverMapGeocodeResponse.java
+++ src/main/java/server/mayfill/external/client/naver/dto/NaverMapGeocodeResponse.java
@@ -1,2 +1,31 @@
-package server.mayfill.external.client.naver.dto;public class NaverMapGeocodeResponse {
+package server.mayfill.external.client.naver.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NaverMapGeocodeResponse {
+
+    private List<GeoAddress> geoAddresses;
+
+    @ToString
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GeoAddress {
+        private String roadAddress;
+        private String jibunAddress;
+        private String englishAddress;
+        private String x;
+        private String y;
+        private double distance;
+    }
+
 }
