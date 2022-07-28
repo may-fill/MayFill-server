7b808153c4cc59a65f9f27e43994b0c5bd6128f8
chanwoo <chocw0402@gmail.com>
2022-07-28 16:10:34 +0900

    [Refactor] HttpHeaderUtils 코드 리팩토링

--- src/main/java/server/mayfill/external/client/kakao/KakaoApiClient.java
+++ src/main/java/server/mayfill/external/client/kakao/KakaoApiClient.java
@@ -3,12 +3,13 @@ package server.mayfill.external.client.kakao;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import server.mayfill.common.util.HttpHeaderUtils;
 import server.mayfill.external.client.kakao.dto.KakaoAuthResponse;
 
 @FeignClient(name = "kakaoAuthApiClient", url = "https://kapi.kakao.com")
 public interface KakaoApiClient {
 
     @GetMapping("/v2/user/me")
-    KakaoAuthResponse getKakaoUserProfile(@RequestHeader("Authorization") String accessToken);
+    KakaoAuthResponse getKakaoUserProfile(@RequestHeader(HttpHeaderUtils.AUTH_HEADER) String accessToken);
 
 }
