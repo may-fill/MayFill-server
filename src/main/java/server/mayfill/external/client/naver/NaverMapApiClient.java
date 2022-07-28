15857b007557c311d3255407b5e7e66c67cb23e4
chanwoo <chocw0402@gmail.com>
2022-07-28 16:12:18 +0900

    [Feat] naver map api

--- src/main/java/server/mayfill/external/client/naver/NaverMapApiClient.java
+++ src/main/java/server/mayfill/external/client/naver/NaverMapApiClient.java
@@ -1,7 +1,18 @@
 package server.mayfill.external.client.naver;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import server.mayfill.common.util.YamlPropertySourceFactory;
+import server.mayfill.external.client.naver.dto.NaverMapGeocodeResponse;
+import server.mayfill.external.config.naver.NaverRequestHeaderConfig;
 
-@FeignClient(name = "naverApiClient")
-public interface NaverApiClient {
+@FeignClient(name = "naverMapApiClient", url = "https://naveropenapi.apigw.ntruss.com", configuration = NaverRequestHeaderConfig.class)
+@PropertySource(value = "classpath:application-naver.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
+public interface NaverMapApiClient {
+
+    @GetMapping(value = "/map-geocode/v2/geocode")
+    NaverMapGeocodeResponse getGeocode(@RequestParam String query);
+
 }
