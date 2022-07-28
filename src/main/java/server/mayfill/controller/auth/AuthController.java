6899494aa7bf2da9b34e3381101510465e72f0d6
chanwoo <chocw0402@gmail.com>
2022-07-28 16:03:39 +0900

    [Fix] token 생성 service 레이어로 위임

--- src/main/java/server/mayfill/controller/auth/AuthController.java
+++ src/main/java/server/mayfill/controller/auth/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import server.mayfill.common.dto.ApiResponse;
 import server.mayfill.common.exception.ResponseResult;
-import server.mayfill.common.util.JwtUtils;
 import server.mayfill.controller.auth.dto.request.LoginRequest;
 import server.mayfill.controller.auth.dto.request.TokenRequest;
 import server.mayfill.controller.auth.dto.response.LoginResponse;
@@ -25,14 +24,14 @@ public class AuthController {
 
     private final AuthServiceProvider authServiceProvider;
     private final CreateTokenService createTokenService;
-    private final JwtUtils jwtProvider;
 
     @ApiOperation("소셜 로그인 - 회원가입 및 로그인 페이지")
     @PostMapping("/v1/auth/login")
     public ApiResponse<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         AuthService authService = authServiceProvider.getAuthService(request.getSocialType());
         User user = authService.socialLogin(request.toServiceDto());
-        return ApiResponse.success(LoginResponse.of(user, jwtProvider.createTokenByUserId(user.getId())));
+        TokenResponseDto tokenInfo = createTokenService.createTokenInfo(user.getId());
+        return ApiResponse.success(LoginResponse.of(user, tokenInfo));
     }
 
     @ApiOperation("토큰 만료 시 엑세스 토큰 재발급 요청")
