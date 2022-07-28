7cc2a7efaa24be6717e5fc79277896fccb5b238d
chanwoo <chocw0402@gmail.com>
2022-07-28 16:00:58 +0900

    [Add] 랜덤 닉네임 추가

--- src/main/java/server/mayfill/common/util/RandomNicknameProviderUtils.java
+++ src/main/java/server/mayfill/common/util/RandomNicknameProviderUtils.java
@@ -11,8 +11,8 @@ import java.util.List;
 public class RandomNicknameProviderUtils {
 
     public static String getRandomNickname() {
-        List<String> nick = new ArrayList<>(List.of("개열받은 ", "미친 개딱딱한 돌빵먹는 ", "호구잡히는 ", "악플다는 ", "평화주의자 ", "급똥 마려운 ", "분노조절장애 "));
-        List<String> name = new ArrayList<>(List.of("스탈린", "호갱", "김삿갓", "신사임당", "나이팅게일", "히틀러", "간디", "나폴레옹", "귀도 반 로섬"));
+        List<String> nick = new ArrayList<>(List.of("개열받은 ", "미친 개딱딱한 돌빵먹는 ", "호구잡히는 ", "악플다는 ", "평화주의자 ", "급똥 마려운 ", "분노 조절 장애 ", "분노 조절 마스터 "));
+        List<String> name = new ArrayList<>(List.of("스탈린", "호갱", "김삿갓", "신사임당", "나이팅게일", "히틀러", "간디", "나폴레옹", "귀도 반 로섬", "김윤서", "이남걸", "조찬우"));
         Collections.shuffle(nick);
         Collections.shuffle(name);
         return nick.get(0) + name.get(0);
