a6d8c63d511db0fcbb0a1aa7034e68ce8de3906e
chanwoo <chocw0402@gmail.com>
2022-07-28 16:21:54 +0900

    [Style] test 세팅

--- src/test/java/server/mayfill/service/auth/CreateTokenServiceTest.java
+++ src/test/java/server/mayfill/service/auth/CreateTokenServiceTest.java
@@ -1,4 +1,17 @@
+package server.mayfill.service.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import server.mayfill.common.util.JwtUtils;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class CreateTokenServiceTest {
-  
+
+    @Autowired
+    private JwtUtils jwtProvider;
+
+
+
 }
--- src/test/java/server/mayfill/service/user/UserServiceTest.java
+++ src/test/java/server/mayfill/service/user/UserServiceTest.java
@@ -1,4 +1,30 @@
+package server.mayfill.service.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import server.mayfill.domain.user.User;
+import server.mayfill.domain.user.enumerate.SocialType;
+import server.mayfill.domain.user.repository.UserRepository;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class UserServiceTest {
-  
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    void 유저의_후기가_정상적으로_등록() {
+        // given
+        User user = User.newInstance("socialId", SocialType.KAKAO, "닉네임");
+        User saved = userRepository.save(user);
+
+        // when
+
+        // then
+    }
+
 }
