99be15229f15223f95c149c975bb1182a51b81c4
chanwoo <chocw0402@gmail.com>
2022-07-31 20:30:38 +0900

    [Feat] AWS S3 bucket image file upload provider

--- src/main/java/server/mayfill/service/image/provider/UploadProvider.java
+++ src/main/java/server/mayfill/service/image/provider/UploadProvider.java
@@ -1,2 +1,22 @@
-package server.mayfill.service.image.provider;public class UploadProvider {
+package server.mayfill.service.image.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import server.mayfill.service.image.client.S3FileStorageClient;
+import server.mayfill.service.image.provider.dto.request.UploadFileRequest;
+
+@Component
+@RequiredArgsConstructor
+public class UploadProvider {
+
+    private final S3FileStorageClient fileStorageClient;
+
+    public String uploadFile(UploadFileRequest request, MultipartFile file) {
+        request.validateAvailableContentType(file.getContentType());
+        String fileName = request.getFileNameWithBucketDirectory(file.getOriginalFilename());
+        fileStorageClient.uploadFile(file, fileName);
+        return fileStorageClient.getFileUrl(fileName);
+    }
+
 }
