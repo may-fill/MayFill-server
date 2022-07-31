0e0ffabcd15167c2cdafcd354477b57d26937c58
chanwoo <chocw0402@gmail.com>
2022-07-31 19:55:37 +0900

    [Feat] 파일 형식 enum

--- src/main/java/server/mayfill/common/type/FileContentType.java
+++ src/main/java/server/mayfill/common/type/FileContentType.java
@@ -1,2 +1,31 @@
-package server.mayfill.common.type;public enum FileContentType {
+package server.mayfill.common.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import server.mayfill.common.exception.custom.ValidationException;
+
+import static server.mayfill.common.exception.ResponseResult.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum FileContentType {
+
+    IMAGE("image"),
+    ;
+
+    private final String prefix;
+
+    public void validateAvailableContentType(String contentType) {
+        if (contentType != null && contentType.contains(SEPARATOR) && prefix.equals(getContentTypePrefix(contentType))) {
+            return;
+        }
+        throw new ValidationException(String.format("허용되지 않은 파일 형식 (%s) 입니다", contentType), FORBIDDEN_FILE_TYPE_EXCEPTION);
+    }
+
+    private static String getContentTypePrefix(String contentType) {
+        return contentType.split(SEPARATOR)[0];
+    }
+
+    private static final String SEPARATOR = "/";
+
 }
