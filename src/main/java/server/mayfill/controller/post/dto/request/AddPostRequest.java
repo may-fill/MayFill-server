387366fef044dfddc61fb155d3a1be9ee76c826e
chanwoo <chocw0402@gmail.com>
2022-07-31 20:11:38 +0900

    [Feat] 인증 포스트 request dto

--- src/main/java/server/mayfill/controller/post/dto/request/AddPostRequest.java
+++ src/main/java/server/mayfill/controller/post/dto/request/AddPostRequest.java
@@ -1,2 +1,25 @@
-package server.mayfill.controller.post.dto.request;public class AddPostRequest {
+package server.mayfill.controller.post.dto.request;
+
+import lombok.*;
+import server.mayfill.domain.post.Post;
+import server.mayfill.domain.user.User;
+
+import javax.validation.constraints.NotBlank;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AddPostRequest {
+
+    @NotBlank(message = "{post.title.notBlank}")
+    private String title;
+
+    @NotBlank(message = "{post.contents.notBlank}")
+    private String contests;
+
+    public Post toPostEntity(User user, String imageUrl) {
+        return Post.of(user, imageUrl, title, contests);
+    }
+
 }
