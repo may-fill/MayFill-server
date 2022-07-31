de0b568b7f2b3471193aa0e35aba17135712dda6
chanwoo <chocw0402@gmail.com>
2022-07-31 19:52:19 +0900

    [Feat] 서비스 자체 uuid 제공 utils

--- src/main/java/server/mayfill/common/util/UuidUtils.java
+++ src/main/java/server/mayfill/common/util/UuidUtils.java
@@ -1,2 +1,16 @@
-package server.mayfill.common.util;public class UuidUtils {
+package server.mayfill.common.util;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class UuidUtils {
+
+    private static final String VERSION = "v1";
+
+    public static String generate() {
+        return String.format("%s-%s", VERSION, UUID.randomUUID().toString());
+    }
+
 }
