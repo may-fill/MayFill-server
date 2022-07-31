0bf6b8848b7c2f3688c32ea71f50dfcfacc1e561
chanwoo <chocw0402@gmail.com>
2022-07-31 20:31:04 +0900

    [Feat] image upload file request dto

--- src/main/java/server/mayfill/service/image/provider/dto/request/ImageUploadFileRequest.java
+++ src/main/java/server/mayfill/service/image/provider/dto/request/ImageUploadFileRequest.java
@@ -1,2 +1,25 @@
-package server.mayfill.service.image.provider.dto.request;public class ImageUploadFileRequest {
+package server.mayfill.service.image.provider.dto.request;
+
+import lombok.*;
+import server.mayfill.common.type.FileType;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ImageUploadFileRequest implements UploadFileRequest {
+
+    private static final String SEPARATOR = "/";
+    private static final String IMAGE_CONTENT_TYPE_TYPE = "image";
+
+    private FileType type;
+
+    public static ImageUploadFileRequest of(FileType type) {
+        return new ImageUploadFileRequest(type);
+    }
+
+    public String getFileNameWithBucketDirectory(String originalFilename) {
+        return type.createUniqueFileNameWithExtension(originalFilename);
+    }
+
 }
--- src/main/java/server/mayfill/service/image/provider/dto/request/UploadFileRequest.java
+++ src/main/java/server/mayfill/service/image/provider/dto/request/UploadFileRequest.java
@@ -1,2 +1,17 @@
-package server.mayfill.service.image.provider.dto.request;public interface UploadFileRequest {
+package server.mayfill.service.image.provider.dto.request;
+
+import server.mayfill.common.type.FileType;
+
+public interface UploadFileRequest {
+
+    FileType getType();
+
+    default void validateAvailableContentType(String contentType) {
+        getType().validateAvailableContentType(contentType);
+    }
+
+    default String getFileNameWithBucketDirectory(String originalFilename) {
+        return getType().createUniqueFileNameWithExtension(originalFilename);
+    }
+
 }
