c629d761d57fdb43be8c1683e721e87e85709536
chanwoo <chocw0402@gmail.com>
2022-07-31 20:31:25 +0900

    [Feat] post service

--- src/main/java/server/mayfill/service/post/PostService.java
+++ src/main/java/server/mayfill/service/post/PostService.java
@@ -1,2 +1,29 @@
-package server.mayfill.service.post;public class PostService {
+package server.mayfill.service.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import server.mayfill.common.type.FileType;
+import server.mayfill.controller.post.dto.request.AddPostRequest;
+import server.mayfill.domain.post.repository.PostRepository;
+import server.mayfill.domain.user.repository.UserRepository;
+import server.mayfill.service.image.provider.UploadProvider;
+import server.mayfill.service.image.provider.dto.request.ImageUploadFileRequest;
+import server.mayfill.service.user.UserServiceUtils;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final UploadProvider uploadProvider;
+
+    @Transactional
+    public void addPostWithImage(AddPostRequest request, MultipartFile imageFile, Long userId) {
+        String imageUrl = uploadProvider.uploadFile(ImageUploadFileRequest.of(FileType.POST_IMAGE), imageFile);
+        postRepository.save(request.toPostEntity(UserServiceUtils.findUserById(userRepository, userId), imageUrl));
+    }
+
 }
