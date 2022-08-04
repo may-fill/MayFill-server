24ef1e1e7b578b596fee7431a97c6b56b4a92479
chanwoo <chocw0402@gmail.com>
2022-08-04 20:19:31 +0900

    [Add] post 연관관계 설정, 회원 등급 업그레이드 로직

--- src/main/java/server/mayfill/domain/user/User.java
+++ src/main/java/server/mayfill/domain/user/User.java
@@ -4,11 +4,15 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import server.mayfill.domain.common.AuditingTimeEntity;
+import server.mayfill.domain.post.Post;
 import server.mayfill.domain.user.embedded.SocialInfo;
 import server.mayfill.domain.user.enumerate.SocialType;
+import server.mayfill.domain.user.enumerate.UserGrade;
 import server.mayfill.domain.user.enumerate.UserStatus;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -30,18 +34,48 @@ public class User extends AuditingTimeEntity {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
 
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
+    private UserGrade grade;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Post> posts = new ArrayList<>();
+
     private User(String socialId, SocialType socialType, String nickname) {
         this.socialInfo = SocialInfo.of(socialId, socialType);
         this.nickname = nickname;
         this.status = UserStatus.ACTIVE;
+        this.grade = UserGrade.SQUIRE;
     }
 
     public static User newInstance(String socialId, SocialType socialType, String nickname) {
         return new User(socialId, socialType, nickname);
     }
 
+    public void addPosts(Post post) {
+        this.posts.add(post);
+    }
+
     public void changeNickname(String nickname) {
         this.nickname = nickname;
+    }
+
+    public void changeGrade(int count) {
+        if (count == 5) {
+            this.grade = UserGrade.BARON;
+        }
+        if (count == 10) {
+            this.grade = UserGrade.EARL;
+        }
+        if (count == 19) {
+            this.grade = UserGrade.DUKE;
+        }
+        if (count == 32) {
+            this.grade = UserGrade.LORD;
+        }
+        if (count == 53) {
+            this.grade = UserGrade.KING;
+        }
     }
 
 }
