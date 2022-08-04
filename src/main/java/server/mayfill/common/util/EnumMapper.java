8c5438337154244820d65add40fa549f05b29d03
chanwoo <chocw0402@gmail.com>
2022-08-04 20:11:36 +0900

    [Feat] Enum Mapper utils

--- src/main/java/server/mayfill/common/util/EnumMapper.java
+++ src/main/java/server/mayfill/common/util/EnumMapper.java
@@ -1,2 +1,31 @@
-package server.mayfill.common.util;public class EnumMapper {
+package server.mayfill.common.util;
+
+import server.mayfill.common.model.EnumModel;
+import server.mayfill.common.model.EnumValue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EnumMapper {
+
+    private final Map<String, List<EnumValue>> factory = new HashMap<>();
+
+    private List<EnumValue> toEnumValues(Class<? extends EnumModel> e) {
+        return Arrays.stream(e.getEnumConstants())
+                .map(EnumValue::new)
+                .collect(Collectors.toList());
+    }
+
+    public void put(String key, Class<? extends EnumModel> e) {
+        factory.put(key, toEnumValues(e));
+    }
+
+    public Map<String, List<EnumValue>> getAll() {
+        return factory;
+    }
+
+
 }
