1c211fd293891a9bf6644feb29d745c7e1d55b9b
chanwoo <chocw0402@gmail.com>
2022-08-04 20:17:13 +0900

    [Feat] 인증 포스트 등록, 인증 조회, 모든 포스트 조회 controller

--- src/main/java/server/mayfill/controller/post/PostController.java
+++ src/main/java/server/mayfill/controller/post/PostController.java
@@ -2,19 +2,22 @@ package server.mayfill.controller.post;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import server.mayfill.common.dto.ApiResponse;
-import server.mayfill.common.exception.ResponseResult;
 import server.mayfill.config.interceptor.Auth;
 import server.mayfill.config.resolver.LoginUserId;
 import server.mayfill.controller.post.dto.request.AddPostRequest;
 import server.mayfill.service.post.PostService;
+import server.mayfill.service.post.dto.response.AllPostResponse;
+import server.mayfill.service.post.dto.response.GradeResponse;
+import server.mayfill.service.post.dto.response.MyPostResponse;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
+import java.util.List;
+
+import static server.mayfill.common.exception.ResponseResult.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,11 +28,24 @@ public class PostController {
     @ApiOperation("[인증] 인증 페이지 - 새로운 인증 등록")
     @Auth
     @PostMapping("/v1/certification-post")
-    public ApiResponse<ResponseResult> addPostWithImage(@Valid AddPostRequest request,
-                                                        @RequestPart MultipartFile image,
-                                                        @ApiIgnore @LoginUserId Long userId) {
-        postService.addPostWithImage(request, image, userId);
-        return ApiResponse.success(ResponseResult.SUCCESS_CREATED_CERTIFICATION_POST);
+    public ApiResponse<GradeResponse> addPostWithImage(@Valid AddPostRequest request,
+                                                       @RequestPart MultipartFile image,
+                                                       @ApiIgnore @LoginUserId Long userId) {
+        return ApiResponse.success(SUCCESS_CREATED_CERTIFICATION_POST, postService.addPostWithImage(request, image, userId));
+    }
+
+    @ApiOperation("[인증] 인증 페이지 - 개인 인증 포스트 조회")
+    @Auth
+    @GetMapping("/v1/certification-post")
+    public ApiResponse<List<MyPostResponse>> retrieveMyPost(@ApiIgnore @LoginUserId Long userId) {
+        return ApiResponse.success(OK_SEARCH_MY_POST, postService.retrieveMyPost(userId));
+    }
+
+    @ApiOperation("[인증] 둘러보기 페이지 - 모든 유저의 인증 포스트 조회")
+    @Auth
+    @GetMapping("/v1/certification-post/all")
+    public ApiResponse<List<AllPostResponse>> retrieveAllPost() {
+        return ApiResponse.success(OK_SEARCH_ALL_POST ,postService.retrieveAllPost());
     }
 
 }
