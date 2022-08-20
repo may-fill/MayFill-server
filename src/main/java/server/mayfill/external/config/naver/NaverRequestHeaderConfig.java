b22a46aedeaa1e587dd5b07e885f2da68e8552cf
chanwoo <chocw0402@gmail.com>
2022-08-20 20:31:17 +0900

    [fix]

--- src/main/java/server/mayfill/external/config/naver/NaverRequestHeaderConfig.java
+++ src/main/java/server/mayfill/external/config/naver/NaverRequestHeaderConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import server.mayfill.common.util.HttpHeaderUtils;
 import server.mayfill.common.util.YamlPropertySourceFactory;
 
 @Configuration
 @PropertySource(value = "classpath:application-naver.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
 public class NaverRequestHeaderConfig {
+
+    private static final String NAVER_API_KEY_ID = "X-NCP-APIGW-API-KEY-ID";
+    private static final String NAVER_API_KEY = "X-NCP-APIGW-API-KEY";
 
     @Value("${naver.client.id}")
     private String clientId;
@@ -21,8 +23,8 @@ public class NaverRequestHeaderConfig {
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
-            requestTemplate.header(HttpHeaderUtils.X_NCP_APIGW_API_KEY_ID, clientId);
-            requestTemplate.header(HttpHeaderUtils.X_NCP_APIGW_API_KEY, clientSecret);
+            requestTemplate.header(NAVER_API_KEY_ID, clientId);
+            requestTemplate.header(NAVER_API_KEY, clientSecret);
         };
     }
 
