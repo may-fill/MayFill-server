736cf681c32a324c64ed72c086e8158918209951
chanwoo <chocw0402@gmail.com>
2022-08-20 21:28:25 +0900

    [feat] tag 엔티티

--- src/main/java/server/mayfill/domain/store/entity/Tag.java
+++ src/main/java/server/mayfill/domain/store/entity/Tag.java
@@ -1,2 +1,23 @@
-package server.mayfill.domain.store.entity;public class Tag {
+package server.mayfill.domain.store.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import server.mayfill.domain.store.entity.enumerate.TagName;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private TagName tagName;
+
 }
