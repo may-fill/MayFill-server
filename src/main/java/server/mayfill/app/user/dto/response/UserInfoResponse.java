7f77f872b08bf7219e1c2e51181a7e61a0bbee7f
chanwoo <chocw0402@gmail.com>
2022-09-02 13:31:54 +0900

    [feat] 유저 정보 조회 response dto

--- src/main/java/server/mayfill/app/user/dto/response/UserInfoResponse.java
+++ src/main/java/server/mayfill/app/user/dto/response/UserInfoResponse.java
@@ -1,2 +1,19 @@
-package server.mayfill.app.user.dto.response;public class UserInfoResponse {
+package server.mayfill.app.user.dto.response;
+
+import lombok.*;
+import server.mayfill.domain.user.entity.enumerate.UserGrade;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserInfoResponse {
+
+    private String nickname;
+    private UserGrade grade;
+
+    public static UserInfoResponse of(String nickname, UserGrade grade) {
+        return new UserInfoResponse(nickname, grade);
+    }
+
 }
